efd44fc89d59c846176ce7073b512fbe947505d9
Radek Czajka <radekczajka@nowoczesnapolska.org.pl>
2013-12-05 16:06:23 +0100

    fixes

--- src/copyspeak/settings/locale.py
+++ src/copyspeak/settings/locale.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 LANGUAGES = (
-    ('pl', u'polski'),
-#    ('en', u'English'),
+    ('en', u'English'),
+#    ('pl', u'polski'),
 )
 
 # Local time zone for this installation. Choices can be found here:
@@ -16,15 +16,15 @@ TIME_ZONE = None
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'pl'
+LANGUAGE_CODE = 'en'
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale.
-USE_L10N = True
+USE_L10N = False
 
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
